41f74ef0fb7f823d4f57ed0c80a81d2dcbc96f62
Jameel Aledrees <165811729+Jameel-Aledrees@users.noreply.github.com>
2025-12-18 19:00:08 +0300

    main.py

--- main.py
+++ main.py
@@ -9,24 +9,20 @@ def print_header(title):
     print("=" * 60)
 
 
-# ---------- ЧТЕНИЕ YAML ----------
 with open("schedule.yaml", "r", encoding="utf-8") as file:
     lines = file.readlines()
 
-# ---------- ПАРСИНГ ----------
 parser = YAMLParser(lines)
 расписание = parser.parse()
 
 print_header("ЭТАП 1. ДЕСЕРИАЛИЗАЦИЯ YAML → ОБЪЕКТНАЯ МОДЕЛЬ")
 pprint(расписание, width=100)
 
-# ---------- СЕРИАЛИЗАЦИЯ В HCL ----------
 hcl_text = serialize_to_hcl(расписание)
 
 print_header("ЭТАП 2. СЕРИАЛИЗАЦИЯ ОБЪЕКТНОЙ МОДЕЛИ → HCL")
 print(hcl_text)
 
-# ---------- ЗАПИСЬ В ФАЙЛ ----------
 with open("schedule.hcl", "w", encoding="utf-8") as file:
     file.write(hcl_text)
 
